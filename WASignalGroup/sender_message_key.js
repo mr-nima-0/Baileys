645155cfc539f7e3140ee3e78600ca39d4536c46
BOTCAHX <109553047+hostinger-bot@users.noreply.github.com>
2023-06-06 00:33:55 +0700

    update

--- WASignalGroup/sender_message_key.js
+++ WASignalGroup/sender_message_key.js
@@ -1,39 +1,43 @@
 const { deriveSecrets } = require('libsignal/src/crypto');
+
 class SenderMessageKey {
-    iteration = 0;
+  constructor(iteration, seed) {
+    this.iteration = iteration;
+    this.seed = seed;
+    this.iv = Buffer.alloc(0);
+    this.cipherKey = Buffer.alloc(0);
+    this.generateKeys();
+  }
 
-    iv = Buffer.alloc(0);
+  generateKeys() {
+    const derivative = deriveSecrets(
+      this.seed,
+      Buffer.alloc(32),
+      Buffer.from('WhisperGroup')
+    );
+    const keys = new Uint8Array(32);
+    keys.set(new Uint8Array(derivative[0].slice(16)));
+    keys.set(new Uint8Array(derivative[1].slice(0, 16)), 16);
+    this.iv = Buffer.from(derivative[0].slice(0, 16));
+    this.cipherKey = Buffer.from(keys.buffer);
+  }
 
-    cipherKey = Buffer.alloc(0);
+  getIteration() {
+    return this.iteration;
+  }
 
-    seed = Buffer.alloc(0);
+  getIv() {
+    return this.iv;
+  }
 
-    constructor(iteration, seed) {
-        const derivative = deriveSecrets(seed, Buffer.alloc(32), Buffer.from('WhisperGroup'));
-        const keys = new Uint8Array(32);
-        keys.set(new Uint8Array(derivative[0].slice(16)));
-        keys.set(new Uint8Array(derivative[1].slice(0, 16)), 16);
-        this.iv = Buffer.from(derivative[0].slice(0, 16));
-        this.cipherKey = Buffer.from(keys.buffer);
+  getCipherKey() {
+    return this.cipherKey;
+  }
 
-        this.iteration = iteration;
-        this.seed = seed;
-    }
-
-    getIteration() {
-        return this.iteration;
-    }
-
-    getIv() {
-        return this.iv;
-    }
-
-    getCipherKey() {
-        return this.cipherKey;
-    }
-
-    getSeed() {
-        return this.seed;
-    }
+  getSeed() {
+    return this.seed;
+  }
 }
+
 module.exports = SenderMessageKey;
+
