028609506826188a43894a2a5aebd2f7f012804f
BOTCAHX <109553047+hostinger-bot@users.noreply.github.com>
2023-06-06 00:49:17 +0700

    Update sender_message_key.js

--- WASignalGroup/sender_message_key.js
+++ WASignalGroup/sender_message_key.js
@@ -1,43 +1,40 @@
 const { deriveSecrets } = require('libsignal/src/crypto');
-
 class SenderMessageKey {
-  constructor(iteration, seed) {
-    this.iteration = iteration;
-    this.seed = seed;
-    this.iv = Buffer.alloc(0);
-    this.cipherKey = Buffer.alloc(0);
-    this.generateKeys();
-  }
+    iteration = 0;
 
-  generateKeys() {
-    const derivative = deriveSecrets(
-      this.seed,
-      Buffer.alloc(32),
-      Buffer.from('WhisperGroup')
-    );
-    const keys = new Uint8Array(32);
-    keys.set(new Uint8Array(derivative[0].slice(16)));
-    keys.set(new Uint8Array(derivative[1].slice(0, 16)), 16);
-    this.iv = Buffer.from(derivative[0].slice(0, 16));
-    this.cipherKey = Buffer.from(keys.buffer);
-  }
+    iv = Buffer.alloc(0);
 
-  getIteration() {
-    return this.iteration;
-  }
+    cipherKey = Buffer.alloc(0);
 
-  getIv() {
-    return this.iv;
-  }
+    seed = Buffer.alloc(0);
 
-  getCipherKey() {
-    return this.cipherKey;
-  }
+    constructor(iteration, seed) {
+        const derivative = deriveSecrets(seed, Buffer.alloc(32), Buffer.from('WhisperGroup'));
+        const keys = new Uint8Array(32);
+        keys.set(new Uint8Array(derivative[0].slice(16)));
+        keys.set(new Uint8Array(derivative[1].slice(0, 16)), 16);
+        this.iv = Buffer.from(derivative[0].slice(0, 16));
+        this.cipherKey = Buffer.from(keys.buffer);
 
-  getSeed() {
-    return this.seed;
-  }
+        this.iteration = iteration;
+        this.seed = seed;
+    }
+
+    getIteration() {
+        return this.iteration;
+    }
+
+    getIv() {
+        return this.iv;
+    }
+
+    getCipherKey() {
+        return this.cipherKey;
+    }
+
+    getSeed() {
+        return this.seed;
+    }
 }
-
-module.exports = SenderMessageKey;
-
+module.exports = SenderMessageKey
+  
